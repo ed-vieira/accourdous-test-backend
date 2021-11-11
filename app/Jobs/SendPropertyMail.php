a5f4ee937c3922a50cbf72f90655ae6663bf93f6
ed-vieira <edfelip@gmail.com>
2021-11-11 00:13:05 -0300

    email job

---
+++ app/Jobs/SendPropertyMail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use App\Models\Property;
+use App\Mail\PropertyContracted as Email;
+use Mail;
+
+class SendPropertyMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+
+    public $property;
+
+    public $tries = 5;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Property $property)
+    {
+        $this->property = $property;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->property->email = 'edfelip@gmail.com';
+        Mail::to($this->property->email)
+            ->send(new Email($this->property));
+    }
+}
