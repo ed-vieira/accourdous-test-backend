f4bb231f84875771713665a0f2246f36ac779b88
ed-vieira <edfelip@gmail.com>
2021-11-11 00:14:47 -0300

    factories

--- database/factories/PropertyFactory.php
+++ database/factories/PropertyFactory.php
@@ -7,6 +7,15 @@ use Faker\Generator as Faker;
 
 $factory->define(Property::class, function (Faker $faker) {
     return [
-        //
+        'title' =>  $faker->sentence(3),
+        'description' =>  $faker->sentence(8),
+        'email' =>  $faker->safeEmail,
+        'cep' =>   $faker->postcode,
+        'state' =>  $faker->stateAbbr,
+        'city' =>  $faker->city,
+        'district' =>  $faker->citySuffix,
+        'street' =>  $faker->streetName,
+        'number' =>  $faker->numberBetween(1, 300),
+        'complement' =>  $faker->sentence(10),
     ];
 });
